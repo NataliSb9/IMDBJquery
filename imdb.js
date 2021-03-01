bfe6bd40297bb620812ee8d86d3dc7bcbf995676
NataliSb9 <nsainzbarroso@gmail.com>
2021-03-02 00:18:43 +0100

    almacenamiento local

--- imdb.js
+++ imdb.js
@@ -65,17 +65,63 @@ class Movie {
     console.log("Genre: " + this.genre);
     console.log("*******************************");
   }
-  getInfoHtml() {
+  
+
+}
+
+class Imdb {
+  constructor() {
+    if(this.getPelisArray() == null){
+      localStorage.setItem("peliculas",JSON.stringify([]));
+    }
+  }
+  
+
+  getInfoHtml2(){
+     let peliculasArray = this.getPelisArray()
+     let resultado = ""
+     for(let i = 0; i< peliculasArray.length; i++){
+        resultado += this.getMovieInfoHtml(peliculasArray[i]) 
+     }
+     return resultado;
+  }
+
+
+  addMovie() {
+    let movie = new Movie(
+      $("#nameMovie").val(),
+      $("#releaseYear").val(),
+      $("#nationality").val(),
+      $("#genre").val(),
+      $("#img").val(),
+      this.calculateId()
+    );
+    let peliculasArray = this.getPelisArray()
+    peliculasArray.push(movie)
+    localStorage.setItem("peliculas",JSON.stringify(peliculasArray));
+  }
+
+  getPelisArray(){
+    let peliculasJson = localStorage.getItem("peliculas")
+    let peliculasArray = JSON.parse(peliculasJson)
+    return peliculasArray
+  }
+  calculateId(){  
+   return this.getPelisArray().length
+  }
+
+  getMovieInfoHtml(movie) {
+
     return `
         <div id= "cardMovie" class="card" style="width: 15rem">
-          <img src="${this.photo}" class="card-img-top width" alt="" />
+          <img src="${movie.photo}" class="card-img-top width" alt="" />
           <div class="card-body">
-            <h6 class="card-title" style = "text-align: center;">${this.title}</h6>
+            <h6 class="card-title" style = "text-align: center;">${movie.title}</h6>
             <button
             type="button"
             class="btn btn-primary row3"
             data-toggle="modal"
-            data-target="#modal${this.id}"
+            data-target="#modal${movie.id}"
             >
             Info Peli
             </button>
@@ -83,7 +129,7 @@ class Movie {
         </div>
   <div
     class="modal fade"
-    id="modal${this.id}"
+    id="modal${movie.id}"
     data-backdrop="static"
     data-keyboard="false"
     tabindex="-1"
@@ -93,7 +139,7 @@ class Movie {
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-         <h5 class="modal-title">${this.title}</h5>
+         <h5 class="modal-title">${movie.title}</h5>
           <button
             type="button"
             class="close"
@@ -106,9 +152,9 @@ class Movie {
         <div class="modal-body">
           <p>Info de la pelicula:</p>
           <ul>
-            <li>Año de estreno: ${this.releaseYear}</li>
-            <li>Nacionalidad: ${this.nationality}</li>
-            <li>Genero: ${this.genre}</li>
+            <li>Año de estreno: ${movie.releaseYear}</li>
+            <li>Nacionalidad: ${movie.nationality}</li>
+            <li>Genero: ${movie.genre}</li>
           </ul>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">
@@ -119,41 +165,6 @@ class Movie {
       </div>
     </div>
   </div> `
-  }
-
-}
-
-class Imdb {
-  constructor(pelis) {
-    this.peliculas = pelis;
-  }
-  mostrarIMBD() {
-    for (let i = 0; i < this.peliculas.length; i++) {
-      this.peliculas[i].mostrarDatosPeliculas();
-    }
-  }
-  getInfoHtml() {
-    let resultado = "";
-    for (let i = 0; i < this.peliculas.length; i++) {
-      resultado += this.peliculas[i].getInfoHtml();
-    }
-    return resultado;
-  }
-
-  addMovie() {
-    let movie = new Movie(
-      $("#nameMovie").val(),
-      $("#releaseYear").val(),
-      $("#nationality").val(),
-      $("#genre").val(),
-      $("#img").val(),
-      this.calculateId()
-    );
-    this.peliculas.push(movie);
-  }
-
-  calculateId(){
-   return this.peliculas.length
   }
 }
 
@@ -181,17 +192,12 @@ let movie4 = new Movie("The gentlemen",2021,"UK", "Action","img./theGentlemen.jp
 
 let imdb = new Imdb([movie1, movie2,movie3,movie4]);
 
-//Incluir peliculas:
 $(document).ready(function () {
-  $("#pelicula").click(function () {
-    $("#verPelis").html(imdb.getInfoHtml());
+    $("#verPelis").html(imdb.getInfoHtml2());
   });
-});
 
 
-//Formulario
-
-$(document).ready(function () {
+ $(document).ready(function () {
   $("#crearPeli").click(function () {
     imdb.addMovie();
     $("#nameMovie").val(" ");
@@ -200,4 +206,6 @@ $(document).ready(function () {
     $("#genre").val(" ");
     $("#img").val(" ");
   });
-});
+}); 
+
+
